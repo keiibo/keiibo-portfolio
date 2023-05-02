8017c877eca231ff3fcc5a1914ecb16f052639c2
keiibo <keita19981209onepiece@gmail.com>
2023-05-02 18:59:31 +0900

    基本的な設定 ・SCSSをTypeScriptで使用できるように変更 ・Google FontのCaplioraを適用

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./Header/Navbar";
+import Error from "./Component/Error";
+import Home from "./Component/Home";
+import "./Style/App.scss";
 
 function App() {
   return (
-    <div>Hello My Portfolio</div>
-   );
+    <div className="container">
+      <Router>
+        <Navbar /> {/* ナビバーのコンポーネント */}
+        <Routes>
+          {/* ルーティングの制御が行える */}
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/about" element={<About />} /> */}
+          {/* <Route path="/works" element={<Works />} /> */}
+
+          <Route path="*" element={<Error />} />
+          {/* ページが見つからない時に出力されるエラーページ*/}
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
